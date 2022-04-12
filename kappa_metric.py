8bd341ac22fa6916066ff65b70490aa93876ae33
Severin Sadjina <Severin.Sadjina@sintef.no>
2022-04-12 19:47:07 +0200

    Added kappa_metric.estimate_alpha_from_kappa_n() and kappa_metric.estimate_alpha_from_kappa()

--- kappa_metric.py
+++ kappa_metric.py
@@ -56,3 +56,44 @@ def kappa_n(series, n, n_bootstrapping=10000):
     '''
     
     return empirical_kappa(series, 1, n, n_bootstrapping=n_bootstrapping)
+
+def estimate_alpha_from_kappa_n(kappa, n=1):
+    '''
+    Estimates tail exponent alpha from the empirical kappa_n.
+    Based on Kappa values for Student t distribution from
+    "Statistical Consequences of Fat Tails" by Nassim N. Taleb (Table 8.3).
+    '''
+    
+    # Interpolation of kappa values from Table 8.3 in "Statistical Consequences of Fat Tails" by Nassim N. Taleb
+    df_kappa = pd.DataFrame({
+        'alpha': np.linspace(1.25, 4, 11+1),
+        1:   [0.792, 0.647, 0.543, 0.465, 0.406, 0.359, 0.321, 0.29, 0.265, 0.243, 0.225, 0.209],
+        30:  [0.765, 0.609, 0.483, 0.387, 0.316, 0.256, 0.224, 0.191, 0.167, 0.149, 0.13, 0.126],
+        100: [0.756, 0.587, 0.451, 0.352, 0.282, 0.227, 0.189, 0.159, 0.138, 0.121, 0.1, 0.093],
+    }).set_index('alpha').reindex(np.linspace(1.25, 4, 8*11+1)).interpolate()
+    
+    return np.mean((df_kappa[n] - kappa).abs().sort_values().iloc[:2].index.values)
+
+def estimate_alpha_from_kappa(series, n_bootstrapping=int(1e6)):
+    '''
+    Estimates tail exponent alpha by estimating the empirical kappa using bootstrapping with "n_bootstrapping" samples.
+    '''
+    
+    assert n_bootstrapping >= 1, 'Must have at least 1 bootstrap sample'
+    
+    cleaned_series = series.dropna().abs()
+    
+    # Calculate kappas
+    result = []
+    for n in [1, 30, 100]:
+        
+        # Get empirical kappa_n
+        _kappa_n = kappa_n(cleaned_series, n=max([2, n]), n_bootstrapping=int(n_bootstrapping))
+        
+        # Estimate alpha from kappa_n
+        _alpha = estimate_alpha_from_kappa_n(_kappa_n, n=n)
+        
+        result.append(_alpha)
+        
+    # Return mean
+    return np.mean(result)

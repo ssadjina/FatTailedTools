4d87f2e1631d486d8232322b7e2292bab06f992f
Severin Sadjina <severin.sadjina@sintef.no>
2023-01-03 15:26:22 +0100

    alpha.fit_alpha_and_scale_linear_subsampling(): Fixed incorrect assert;

--- alpha.py
+++ alpha.py
@@ -239,9 +239,9 @@ def fit_alpha_and_scale_linear_subsampling(
         assert min_samples < len(abs_series)
         threshold_max = abs_series.iloc[-min_samples]
         assert threshold_max > 0
+        assert (abs_series >= threshold_max).sum() >= min_samples
         threshold_min = abs_series.median()
         assert threshold_min < threshold_max
-        assert abs_series.between(threshold_min, threshold_max, inclusive='both').sum() >= min_samples
         thresholds    = np.linspace(threshold_min, threshold_max, n_fits_per_subsample)
 
         # Set up lists to store results of the linear fits

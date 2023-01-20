5aa4eba6ab208f19507bf3176ac5c60937a469ba
Severin Sadjina <severin.sadjina@sintef.no>
2023-01-20 11:53:27 +0100

    returns: Added 'drop_zeros' to get_returns() and get_log_returns(); Also added more documentation;

--- returns.py
+++ returns.py
@@ -4,9 +4,17 @@ import numpy as np
 
 
 
-def get_returns(series, periods='1d', offset=0):
+def get_returns(series, periods='1d', offset=0, drop_zeros=True):
     '''
-    Calculates the returns from a given Pandas Series using period "periods" with an offset of 'offset' periods.
+    Returns the geometric returns from a given Pandas Series using period "periods" with an offset of 'offset' samples.
+    :param series: Pandas Series holding the time series data of prices.
+    :param periods: Specifies the time period over which to calculate returns (e.g. daily, weekly).
+    :param offset: Number of samples to shift 'series' by before calculating returns. This can be useful to choose the
+    bias when calculating returns over time periods longer than the frequency of the price data.
+    :param drop_zeros: If true, samples where the returns are zero will be dropped before returning. This is useful
+    for avoiding that the results contain a lot of zeros because missing prices were forward-filled before
+    calculating returns.
+    :return: A Pandas Series holding the resulting returns over the specified period with specified sample offset.
     '''
 
     # Drop NAs
@@ -18,6 +26,10 @@ def get_returns(series, periods='1d', offset=0):
     # Calculate returns
     returns = series_resampled/series_resampled.shift(1) - 1
 
+    # Drop zeros introduced by using ffill()
+    if drop_zeros:
+        returns = returns.drop(returns.index[returns == 0])
+
     # Make sure the product of all returns is equal the total return over the period
     series_reconstructed = cleaned_series.iloc[offset] * (1 + returns).cumprod()
     rel_error = (series_reconstructed / series - 1).abs().sum()
@@ -27,9 +39,14 @@ def get_returns(series, periods='1d', offset=0):
 
 
 
-def get_log_returns(series, periods='1d', offset=0):
+def get_log_returns(series, periods='1d', offset=0, drop_zeros=True):
     '''
-    Returns the logarithmic return from a given Pandas Series using period "periods" with an offset of 'offset' periods.
+    Returns the logarithmic returns from a given Pandas Series using period "periods" with an offset of 'offset' samples.
+    :param series: Pandas Series holding the time series data of prices.
+    :param periods: Specifies the time period over which to calculate log returns (e.g. daily, weekly).
+    :param offset: Number of samples to shift 'series' by before calculating log returns. This can be useful to choose the bias when calculating log returns over time periods longer than the frequency of the price data.
+    :param drop_zeros: If true, samples where the log returns are zero will be dropped before returning. This is useful for avoiding that the results contain a lot of zeros because missing prices were forward-filled before calculating log returns.
+    :return: A Pandas Series holding the resulting log returns over the specified period with specified sample offset.
     '''
 
     # Drop NAs
@@ -40,6 +57,10 @@ def get_log_returns(series, periods='1d', offset=0):
 
     # Calculate log returns
     log_returns = np.log(series_resampled/series_resampled.shift(1))
+
+    # Drop zeros introduced by using ffill()
+    if drop_zeros:
+        log_returns = log_returns.drop(log_returns.index[log_returns == 0])
 
     # Make sure the sum of all log returns is equal the total log return over the period
     series_reconstructed = cleaned_series.iloc[offset] * np.exp(log_returns.cumsum())

59a7dff3d4b1e8aca94634a86215a44b430f3b3c
Severin Sadjina <severin.sadjina@sintef.no>
2023-01-30 20:03:02 +0100

    tails: Added rebalance_tails() to use random over- or undersampling to match the number of samples in the left and the right tail;

--- tails.py
+++ tails.py
@@ -31,6 +31,53 @@ def get_both_tails(series):
 
 
 
+def rebalance_tails(series, method='oversampling'):
+    '''
+    Rebalances the tails to make sure the left and the right tail have equal amounts of (notnull) samples.
+    Uses random over- or undersampling with replacement to match minority and majority sides.
+    :param series: Pandas Series that holds the samples (both tails).
+    :param method: Either 'oversampling' (draw samples from minority side to match majority side in size) or
+    'undersampling' (draw samples from majority side to match minority side in size), both with replacement.
+    :return: Returns the rebalances series as a Pandas Series object.
+    '''
+
+    # Sanity check
+    assert method in ['oversampling', 'undersampling']
+
+    # Split samples into left and right tails
+    series_left  = -get_left_tail(series).dropna()
+    series_right =  get_right_tail(series).dropna()
+
+    # Sanity checks
+    assert len(series_left)  > 0
+    assert len(series_right) > 0
+
+    # Identify minority and majority side
+    if len(series_left) < len(series_right):
+        series_minority, series_majority = series_left, series_right
+    else:
+        series_minority, series_majority = series_right, series_left
+
+    # Resample
+    if method == 'oversampling':
+        # Use random oversampling with replacement to match the sample size of the minority side with the majority side
+        series_minority = series_minority.sample(n=len(series_majority), replace=True)
+    elif method == 'undersampling':
+        # Use random undersampling with replacement to match the sample size of the majority side with the minority side
+        series_majority = series_majority.sample(n=len(series_minority), replace=True)
+    else:
+        assert False
+
+    # Assemble new rebalanced series
+    series_rebalanced = pd.concat([series_minority, series_majority], axis=0)
+
+    # Sanity check
+    assert (series_rebalanced < 0).dropna().sum() == (series_rebalanced > 0).dropna().sum()
+
+    return series_rebalanced
+
+
+
 def shoulders_and_tails_t(df, loc, scale):
     '''
     Returns the location of the "shoulders" and the "tails" of a Student t distribution...
